67e79a0210aa2ca75cc05ab3e9918d1c67a1bd86
Banothu Suresh <sureshnayak6281@gmail.com>
2024-10-16 14:14:49 +0530

    chanes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     API_URL: process.env.NODE_ENV === 'production'
-      ? 'https://raw-nonveg-server-1.onrender.com' 
+      ? 'https://raw-nonveg-server-2.onrender.com' 
       : 'http://localhost:5000'
   };
   
